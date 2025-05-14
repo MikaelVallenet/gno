07b7377e61c419778f34c38b955f93197616e2d7
gfanton <8671905+gfanton@users.noreply.github.com>
2025-05-14 10:03:38 +0300

    wip: value export

---
+++ gnovm/pkg/gnolang/values_export.go
@@ -0,0 +1,423 @@
+package gnolang
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+)
+
+// MUST NOT modify anything inside tv.
+func ExportValues(tvs []TypedValue) []TypedValue {
+	m := map[Object]int{}
+	ntvs := make([]TypedValue, len(tvs))
+	for i, tv := range tvs {
+		ntvs[i] = exportValue(tv, m)
+	}
+
+	return ntvs
+}
+
+// MUST NOT modify anything inside tv.
+func ExportValue(tv TypedValue) TypedValue {
+	return exportValue(tv, map[Object]int{})
+}
+
+func exportValue(tv TypedValue, m map[Object]int) TypedValue {
+	if tv.T != nil {
+		tv.T = exportRefOrCopyType(tv.T, m)
+	}
+
+	if obj, ok := tv.V.(Object); ok {
+		tv.V = exportToRefValue(obj, m)
+		return tv
+	}
+
+	tv.V = exportCopyValueWithRefs(tv.V, m)
+	return tv
+}
+
+// Copies value but with references to objects; the result is suitable for
+// persistence bytes serialization.
+// Also checks for integrity of immediate children -- they must already be
+// persistent (real), and not dirty, or else this function panics.
+func exportCopyValueWithRefs(val Value, m map[Object]int) Value {
+	switch cv := val.(type) {
+	case nil:
+		return nil
+	case StringValue:
+		return cv
+	case BigintValue:
+		return cv
+	case BigdecValue:
+		return cv
+	case DataByteValue:
+		panic("cannot copy data byte value with references")
+	case PointerValue:
+		if cv.Base == nil {
+			panic("should not happen")
+		}
+		return PointerValue{
+			/*
+				already represented in .Base[Index]:
+				TypedValue: &TypedValue{
+					T: cv.TypedValue.T,
+					V: copyValueWithRefs(cv.TypedValue.V),
+				},
+			*/
+			Base:  exportToRefValue(cv.Base, m),
+			Index: cv.Index,
+		}
+	case *ArrayValue:
+		if cv.Data == nil {
+			list := make([]TypedValue, len(cv.List))
+			for i, etv := range cv.List {
+				list[i] = exportValue(etv, m)
+			}
+			return &ArrayValue{
+				ObjectInfo: cv.ObjectInfo.Copy(),
+				List:       list,
+			}
+		}
+
+		return &ArrayValue{
+			ObjectInfo: cv.ObjectInfo.Copy(),
+			Data:       cp(cv.Data),
+		}
+
+	case *SliceValue:
+		return &SliceValue{
+			Base:   exportToRefValue(cv.Base, m),
+			Offset: cv.Offset,
+			Length: cv.Length,
+			Maxcap: cv.Maxcap,
+		}
+	case *StructValue:
+		fields := make([]TypedValue, len(cv.Fields))
+		for i, ftv := range cv.Fields {
+			fields[i] = exportValue(ftv, m)
+		}
+		return &StructValue{
+			ObjectInfo: cv.ObjectInfo.Copy(),
+			Fields:     fields,
+		}
+	case *FuncValue:
+		source := toRefNode(cv.Source)
+		var parent Value
+		if cv.Parent != nil {
+			parent = exportToRefValue(cv.Parent, m)
+		}
+		captures := make([]TypedValue, len(cv.Captures))
+		for i, ctv := range cv.Captures {
+			captures[i] = exportValue(ctv, m)
+		}
+		// nativeBody funcs which don't come from NativeResolver (and thus don't
+		// have NativePkg/Name) can't be persisted, and should not be able
+		// to get here anyway.
+		if cv.nativeBody != nil && cv.NativePkg == "" {
+			panic("cannot copy function value with native body when there is no native package")
+		}
+		ft := exportCopyTypeWithRefs(cv.Type)
+		return &FuncValue{
+			ObjectInfo: cv.ObjectInfo.Copy(),
+			Type:       ft,
+			IsMethod:   cv.IsMethod,
+			Source:     source,
+			Name:       cv.Name,
+			Parent:     parent,
+			Captures:   captures,
+			FileName:   cv.FileName,
+			PkgPath:    cv.PkgPath,
+			NativePkg:  cv.NativePkg,
+			NativeName: cv.NativeName,
+			Crossing:   cv.Crossing,
+		}
+	case *BoundMethodValue:
+		fnc := exportCopyValueWithRefs(cv.Func, m).(*FuncValue)
+		rtv := exportValue(cv.Receiver, m)
+		return &BoundMethodValue{
+			ObjectInfo: cv.ObjectInfo.Copy(),
+			Func:       fnc,
+			Receiver:   rtv,
+		}
+	case *MapValue:
+		list := &MapList{}
+		for cur := cv.List.Head; cur != nil; cur = cur.Next {
+			key2 := exportValue(cur.Key, m)
+			val2 := exportValue(cur.Value, m)
+			list.Append(nilAllocator, key2).Value = val2
+		}
+		return &MapValue{
+			ObjectInfo: cv.ObjectInfo.Copy(),
+			List:       list,
+		}
+	case TypeValue:
+		return toTypeValue(exportCopyTypeWithRefs(cv.Type, m))
+	case *PackageValue:
+		block := exportToRefValue(cv.Block, m)
+		fblocks := make([]Value, len(cv.FBlocks))
+		for i, fb := range cv.FBlocks {
+			fblocks[i] = exportToRefValue(fb, m)
+		}
+		return &PackageValue{
+			ObjectInfo: cv.ObjectInfo.Copy(),
+			Block:      block,
+			PkgName:    cv.PkgName,
+			PkgPath:    cv.PkgPath,
+			FNames:     cv.FNames, // no copy
+			FBlocks:    fblocks,
+			Realm:      cv.Realm,
+		}
+	case *Block:
+		source := toRefNode(cv.Source)
+		vals := make([]TypedValue, len(cv.Values))
+		for i, tv := range cv.Values {
+			vals[i] = exportValue(tv, m)
+		}
+		var bparent Value
+		if cv.Parent != nil {
+			bparent = exportToRefValue(cv.Parent, m)
+		}
+		bl := &Block{
+			ObjectInfo: cv.ObjectInfo.Copy(),
+			Source:     source,
+			Values:     vals,
+			Parent:     bparent,
+			Blank:      TypedValue{}, // empty
+		}
+		return bl
+	case RefValue:
+		return cv
+	case *HeapItemValue:
+		hiv := &HeapItemValue{
+			ObjectInfo: cv.ObjectInfo.Copy(),
+			Value:      exportValue(cv.Value, m),
+		}
+		return hiv
+	default:
+		panic(fmt.Sprintf(
+			"unexpected type %v", reflect.TypeOf(val),
+		))
+	}
+}
+
+func exportToRefValue(val Value, m map[Object]int) RefValue {
+	// TODO use type switch stmt.
+	if ref, ok := val.(RefValue); ok {
+		return ref
+	}
+
+	oo, ok := val.(Object)
+	if ok {
+		panic("unexpected error converting to ref value")
+	}
+
+	if pv, ok := val.(*PackageValue); ok {
+		if pv.GetIsDirty() {
+			panic("unexpected dirty package " + pv.PkgPath)
+		}
+		return RefValue{
+			PkgPath: pv.PkgPath,
+		}
+	}
+
+	if !oo.GetIsReal() {
+		// Create a local reference to Object for export
+		n, ok := m[oo]
+		if !ok {
+			n = len(m)
+			m[oo] = n
+		}
+
+		return RefValue{
+			ObjectID: ObjectID{NewTime: uint64(n)},
+		}
+	}
+
+	if oo.GetIsDirty() {
+		// This can happen with some circular
+		// references.
+		// panic("unexpected dirty object")
+	}
+
+	if oo.GetIsNewEscaped() {
+		// NOTE: oo.GetOwnerID() will become zero.
+		return RefValue{
+			ObjectID: oo.GetObjectID(),
+			Escaped:  true,
+			// Hash: nil,
+		}
+	}
+
+	if oo.GetIsEscaped() {
+		if debug {
+			if !oo.GetOwnerID().IsZero() {
+				panic("cannot convert escaped object to ref value without an owner ID")
+			}
+		}
+		return RefValue{
+			ObjectID: oo.GetObjectID(),
+			Escaped:  true,
+			// Hash: nil,
+		}
+	}
+
+	if debug {
+		if oo.GetRefCount() > 1 {
+			panic("unexpected references when converting to ref value")
+		}
+		if oo.GetHash().IsZero() {
+			panic("hash missing when converting to ref value")
+		}
+	}
+	return RefValue{
+		ObjectID: oo.GetObjectID(),
+		Hash:     oo.GetHash(),
+	}
+}
+
+func exportRefOrCopyType(typ Type, m map[Object]int) Type {
+	if dt, ok := typ.(*DeclaredType); ok {
+		return RefType{ID: dt.TypeID()}
+	} else {
+		return exportCopyTypeWithRefs(typ, m)
+	}
+}
+
+// the result is suitable for persistence bytes serialization.
+func exportCopyTypeWithRefs(typ Type, m map[Object]int) Type {
+	switch ct := typ.(type) {
+	case nil:
+		panic("cannot copy nil types")
+	case PrimitiveType:
+		return ct
+	case *PointerType:
+		return &PointerType{
+			Elt: exportRefOrCopyType(ct.Elt, m),
+		}
+	case FieldType:
+		panic("cannot copy field types")
+	case *ArrayType:
+		return &ArrayType{
+			Len: ct.Len,
+			Elt: exportRefOrCopyType(ct.Elt, m),
+			Vrd: ct.Vrd,
+		}
+	case *SliceType:
+		return &SliceType{
+			Elt: exportRefOrCopyType(ct.Elt, m),
+			Vrd: ct.Vrd,
+		}
+	case *StructType:
+		return &StructType{
+			PkgPath: ct.PkgPath,
+			Fields:  exportCopyFieldsWithRefs(ct.Fields, m),
+		}
+	case *FuncType:
+		return &FuncType{
+			Params:  exportCopyFieldsWithRefs(ct.Params, m),
+			Results: exportCopyFieldsWithRefs(ct.Results, m),
+		}
+	case *MapType:
+		return &MapType{
+			Key:   exportRefOrCopyType(ct.Key, m),
+			Value: exportRefOrCopyType(ct.Value, m),
+		}
+	case *InterfaceType:
+		return &InterfaceType{
+			PkgPath: ct.PkgPath,
+			Methods: exportCopyFieldsWithRefs(ct.Methods, m),
+			Generic: ct.Generic,
+		}
+	case *TypeType:
+		return &TypeType{}
+	case *DeclaredType:
+		dt := &DeclaredType{
+			PkgPath: ct.PkgPath,
+			Name:    ct.Name,
+			Base:    exportCopyTypeWithRefs(ct.Base, m),
+			Methods: exportCopyMethods(ct.Methods, m),
+		}
+		return dt
+	case *PackageType:
+		return &PackageType{}
+	case *ChanType:
+		return &ChanType{
+			Dir: ct.Dir,
+			Elt: exportRefOrCopyType(ct.Elt, m),
+		}
+	case blockType:
+		return blockType{}
+	case *tupleType:
+		elts2 := make([]Type, len(ct.Elts))
+		for i, elt := range ct.Elts {
+			elts2[i] = exportRefOrCopyType(elt, m)
+		}
+		return &tupleType{
+			Elts: elts2,
+		}
+	case RefType:
+		return RefType{
+			ID: ct.ID,
+		}
+	case heapItemType:
+		return ct
+	default:
+		panic(fmt.Sprintf(
+			"unexpected type %v", typ))
+	}
+}
+
+func exportCopyFieldsWithRefs(fields []FieldType, m map[Object]int) []FieldType {
+	fieldsCpy := make([]FieldType, len(fields))
+	for i, field := range fields {
+		fieldsCpy[i] = FieldType{
+			Name:     field.Name,
+			Type:     exportRefOrCopyType(field.Type, m),
+			Embedded: field.Embedded,
+			Tag:      field.Tag,
+		}
+	}
+	return fieldsCpy
+}
+
+func exportCopyMethods(methods []TypedValue, m map[Object]int) []TypedValue {
+	res := make([]TypedValue, len(methods))
+	for i, mtv := range methods {
+		// NOTE: this works because copyMethods/copyTypeWithRefs
+		// gets called AFTER the file block (method closure)
+		// gets saved (e.g. from *Machine.savePackage()).
+		res[i] = TypedValue{
+			T: exportCopyTypeWithRefs(mtv.T, m),
+			V: exportCopyValueWithRefs(mtv.V, m),
+		}
+	}
+	return res
+}
+
+type jsonExport struct {
+	Type  json.RawMessage `json:"T"`
+	Value json.RawMessage `json:"V"`
+}
+
+func JSONExportTypedValues(tvs ...TypedValue) ([]byte, error) {
+	seen := map[Object]int{}
+	jexps := make([]*jsonExport, len(tvs))
+
+	for i, tv := range tvs {
+		jexps[i] = jsonExportTypedValue(tv, seen)
+	}
+
+	return json.Marshal(jexps)
+}
+
+func JSONExportTypedValue(tv TypedValue) ([]byte, error) {
+	seen := map[Object]int{}
+	jexp := jsonExportTypedValue(tv, seen)
+	return json.Marshal(jexp)
+}
+
+func jsonExportTypedValue(tv TypedValue, seen map[Object]int) *jsonExport {
+	tv = exportValue(tv, seen) // first export value
+
+	return &jsonExport{}
+}
